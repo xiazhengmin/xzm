8fbda01354147ae9d44b5bb0636c3d357220480b
xiazhengmin <1753455489@qq.com>
2020-11-10 15:40:09 +0800

    Activity

--- app/src/main/java/com/example/xzm/Main2Activity.java
+++ app/src/main/java/com/example/xzm/Main2Activity.java
@@ -6,9 +6,4 @@ import android.os.Bundle;
 
 public class Main2Activity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-    }
 }
